4a58dab0311d4bc602abd6ed1284870d95889369
Rohit Nayyar <Rohitnayyar54@gmail.com>
2023-05-21 16:07:54 -0400

    working code

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,24 +1,93 @@
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  async function getNotes() {
+    try {
+      const response = await fetch("/api/notes", {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (!response.ok) {
+        throw new Error('Error in GET request: ' + response.status);
+      }
+    
+      const data = await response.json();
+      console.log("This is the fetched data:", data);
+    
+      const ul = document.querySelector(".Pendin-Notes");
+      ul.innerHTML = '';
+    
+      data.forEach((note) => {
+        const li = document.createElement("li");
+        li.className = "Pending-Notes-Link";
+        li.innerHTML = `${note.title}`;
+        li.setAttribute("id", `${note.id}`);
+    
+        const img = document.createElement("img");
+        img.src = "./assets/images/trash.png";
+        img.className = "Pending-Notes-Link-Delete";
+        img.onclick = () => handleNoteDelete(note.id, li); // Pass the note ID and li element as arguments
+    
+        ul.appendChild(li);
+        li.appendChild(img);
+      });
+    } catch (error) {
+      console.error('Error in GET request:', error);
+    }
+  }
+  
+  function delEntry(id) {
+    return fetch(`/api/notes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  
+  function handleNoteDelete(id, liElement) {
+    delEntry(id)
+      .then(() => {
+        // Delete the corresponding list item from the UI
+        liElement.remove();
+      })
+      .catch((error) => {
+        console.error('Error deleting note:', error);
+      });
+  }
+  
+function newNote() {
+  const newTitle = document.getElementById("title").value;
+  const newDescription = document.getElementById("text").value;
+
+  const data = {
+    title: newTitle,
+    text: newDescription
+  };
+
+  fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((responseData) => {
+      console.log('Successful POST request:', responseData);
+      getNotes(); // Refresh the notes after successful POST
+    })
+    .catch((error) => {
+      console.error('Error in POST request:', error);
+    });
+}
+
+const save = document.querySelector(".Primary-Nav-Link-None");
+
+save.addEventListener("click", () => {
+  newNote();
+});
+getNotes()
+});
 
 
-// function getNotes(){
-//     fetch('/notes.html', {
-//         method: 'GET',
-//       })
-//         .then(response => {
-//           if (response.ok) {
-//             return response.json();
-//           } else {
-//             throw new Error('Error: ' + response.status);
-//           }
-//         })
-//         .then(data => {
-//           // Process the response data
-//           console.log(data);
-//         })
-//         .catch(error => {
-//           // Handle any errors
-//           console.error('Fetch Error:', error);
-//         });
-// }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,13 +4,14 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-// router.get("/notes",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"../db/db.json"));
+router.get("/api/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../db/db.json"));
 
-// })
+})
 
 
-router.post("/notes", (req, res) => {
+router.post("/api/notes", (req, res) => {
+    console.log(req.body);
     const dbFilePath = path.join(__dirname, '..', 'db', 'db.json');
 
     // Read the existing data from the JSON file
@@ -25,19 +26,44 @@ router.post("/notes", (req, res) => {
     db.push(newNote);
 
     // Write the updated data back to the JSON file
-    fs.writeFileSync(dbFilePath, JSON.stringify(db));
+    fs.writeFileSync(dbFilePath, JSON.stringify(db,null,2));
 
     // Respond to the client with a success message or appropriate status code
     res.status(200).json({ message: 'Note added successfully' });
 });
 
-router.delete("/notes/:id", (req, res) => {
-    const db = require("../db/db.json");
-    const id = req.params.id;
-    const dbNotes = db.filter((note) => note.id !== id);
-    db.splice(id, 1);
-    res.json(dbNotes);
 
+router.delete("/api/notes/:id", (req, res) => {
+  const dbFilePath = path.join(__dirname, '../db/db.json');
+  const id = req.params.id;
+
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading database file:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    let db = JSON.parse(data);
+
+    const dbNotes = db.filter((note) => note.id !== id);
+    const deletedNote = db.find((note) => note.id === id);
+
+    if (!deletedNote) {
+      return res.status(404).json({ error: 'Note not found' });
+    }
+
+    db = dbNotes;
+
+    fs.writeFile(dbFilePath, JSON.stringify(db), (err) => {
+      if (err) {
+        console.error('Error updating database file:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      res.json({ message: 'Note deleted successfully' });
+    });
+  });
 });
+
 
 module.exports = router
