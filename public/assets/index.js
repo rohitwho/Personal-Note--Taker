e915efda2c3f0bc4b10e87d94baab4133ee84297
Rohit Nayyar <Rohitnayyar54@gmail.com>
2023-05-22 22:06:00 -0400

    added Functionality

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,7 +1,20 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
- 
+
+  const show = document.querySelector(".Confused-Guy");
+  const modal = document.querySelector(".modal")
+  const closeModal = document.querySelector(".Got-it")
+
+  show.addEventListener("click", function () {
+
+    modal.showModal()
+  }
+  )
+
+  closeModal.addEventListener("click", function () {
+    modal.close();
+  })
 
 
 
@@ -26,13 +39,13 @@ document.addEventListener('DOMContentLoaded', () => {
         li.className = "Pending-Notes-Link";
         li.innerHTML = `${note.title}`;
         li.setAttribute("id", `${note.id}`);
-        li.addEventListener("click", (e) => {showEntry(note.id,li)})
+        li.addEventListener("click", (e) => { showEntry(note.id, li) })
 
         const img = document.createElement("img");
         img.src = "./assets/images/trash.png";
         img.alt = "DELETE ICON";
         img.className = "Pending-Notes-Link-Delete";
-        img.onclick = () => removeNote(note.id, li); // Pass the note ID and li element as arguments
+        img.onclick = () => removeNote(note.id, li);
 
         ul.appendChild(li);
         li.appendChild(img);
@@ -61,19 +74,19 @@ document.addEventListener('DOMContentLoaded', () => {
           'Content-Type': 'application/json',
         },
       });
-  
+
       if (!response.ok) {
         throw new Error('Request failed with status ' + response.status);
       }
-  
+
       const data = await response.json();
-      
+
       const entry = data.find(entry => entry.id === id);
-      
+
       if (entry) {
         const title = entry.title;
         const text = entry.text;
-    
+
         console.log(title);
         console.log(text);
 
@@ -81,48 +94,32 @@ document.addEventListener('DOMContentLoaded', () => {
         let existingTitle = document.querySelector(".Add-Note-Form-Input");
         let existingText = document.querySelector(".Add-Note-Form-Input-Textarea");
 
-    existingTitle.style.display="none";
-    existingText.style.display="none";
-
-    
-
-let selectTitle = document.querySelector(".Notes-Title");
-let selectText = document.querySelector(".Notes-Text");
-
-selectTitle.innerHTML= title;
-selectTitle.style.padding = "1rem"
-selectTitle.style.borderBottom = "4px solid";
-selectText.innerHTML= text;
-selectText.style.padding = "1rem"
-selectText.style.borderBottom = "10px inset";
+        existingTitle.style.display = "none";
+        existingText.style.display = "none";
 
 
 
+        let selectTitle = document.querySelector(".Notes-Title");
+        let selectText = document.querySelector(".Notes-Text");
 
-// let idk = document.querySelector(".Add-Note-Form")
-//     const h1 = document.createElement("h1");
-//     h1.textContent = title;
-//     h1.style.display="block";
-//     h1.style.color="white";
-//     h1.style.fontSize="30px";
-//     h1.style.padding="10px";
-
-
-// const p = document.createElement("p")
-// p.textContent = text;
-// p.style.display="block";
-// p.style.color="white";
-// p.style.fontSize="20px";
-// p.style.padding="10px";
+        selectTitle.innerHTML = title;
+        selectTitle.style.padding = "1rem"
+        selectTitle.style.margin = "4rem"
+        selectTitle.style.border = "2px solid";
+        selectTitle.style.borderRadius = "10px 10px 90px";
 
 
 
-//     idk.appendChild(h1)
-//     idk.appendChild(p);
+
+        selectText.innerHTML = text;
+        selectText.style.padding = "1rem"
+        selectText.style.fontSize = "1.5rem"
+        selectText.style.border = "2px solid";
+        selectText.style.borderRadius = "10px 10px 90px";
 
 
-        
-  
+
+
       } else {
         console.log(`Entry with ID ${id} not found.`);
       }
@@ -130,9 +127,6 @@ selectText.style.borderBottom = "10px inset";
       console.error('Error in GET request:', error);
     }
   }
-  
-  // TODO: i wanna show the fetched data from show entry into the Text area. 
-
 
 
 
@@ -149,27 +143,19 @@ selectText.style.borderBottom = "10px inset";
 
 
 
-
-
-
-
-
-
-
-
   }
   let clear = document.querySelector(".Primary-Nav-Link").addEventListener("click", createNote);
 
-function createNote(e) {
-  e.preventDefault();
-  location.reload();
-}
+  function createNote(e) {
+    e.preventDefault();
+    location.reload();
+  }
 
 
 
   function newNote() {
-   
-    var  newTitle = document.getElementById("title").value;
+
+    var newTitle = document.getElementById("title").value;
     var newDescription = document.getElementById("text").value;
 
     const data = {
@@ -179,7 +165,7 @@ function createNote(e) {
 
     fetch("/api/notes", {
 
-  
+
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
@@ -189,7 +175,7 @@ function createNote(e) {
       .then((res) => res.json())
       .then((responseData) => {
         console.log('Successful POST request:', responseData);
-        getNotes(); // Refresh the notes after successful POST
+        getNotes();
       })
       .catch((error) => {
         console.error('Error in POST request:', error);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,35 +4,35 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/notes",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../db/db.json"));
+router.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 
 })
 
-router.get("/api/notes/:id",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../db/db.json"));
+router.get("/api/notes/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 })
 
 router.post("/api/notes", (req, res) => {
-    console.log(req.body);
-    const dbFilePath = path.join(__dirname, '..', 'db', 'db.json');
+  console.log(req.body);
+  const dbFilePath = path.join(__dirname, '..', 'db', 'db.json');
 
-    // Read the existing data from the JSON file
-      const db = JSON.parse(fs.readFileSync(dbFilePath));
+  // Read the existing data from the JSON file
+  const db = JSON.parse(fs.readFileSync(dbFilePath));
 
-    let newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    };
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
 
-    db.push(newNote);
+  db.push(newNote);
 
-    // Write the updated data back to the JSON file
-    fs.writeFileSync(dbFilePath, JSON.stringify(db,null,2));
+  // Write the updated data back to the JSON file
+  fs.writeFileSync(dbFilePath, JSON.stringify(db, null, 2));
 
-    // Respond to the client with a success message or appropriate status code
-    res.status(200).json({ message: 'Note added successfully' });
+  // Respond to the client with a success message or appropriate status code
+  res.status(200).json({ message: 'Note added successfully' });
 });
 
 
@@ -70,3 +70,7 @@ router.delete("/api/notes/:id", (req, res) => {
 
 
 module.exports = router
+
+
+
+
